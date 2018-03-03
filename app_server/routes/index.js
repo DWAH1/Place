1cd524fd4606a34c01919bf0b5455db9e7277039
DWAH1 <seregax517@gmail.com>
2018-03-04 00:44:13 +0200

    Adding simple routing
    Added custom controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// Custom controllers
 var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOther = require('../controllers/other');
+
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+router.get('/about', ctrlOther.about);
 
 module.exports = router;
